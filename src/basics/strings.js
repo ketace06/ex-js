c81aefac0579b4fa7a5a33a71bdbce0dd6881701
ketace06 <ismail.omaramin06@gmail.com>
2025-04-07 13:45:28 +0200

    feat: implement string replacement with case sensitivity

--- src/basics/strings.js
+++ src/basics/strings.js
@@ -7,5 +7,24 @@
  * @return {string} the resulting string, with all needle words transformed to newWord
  */
 export function findAndReplacePreservingCase(needle, haystack, newWord) {
-  // Write your code here
+  if (
+    typeof needle !== 'string' ||
+    typeof haystack !== 'string' ||
+    typeof newWord !== 'string'
+  ) {
+    throw new TypeError('All arguments must be strings')
+  }
+  const regex = new RegExp(needle, 'gi')
+
+  return haystack.replace(regex, (match) => {
+    let result = ''
+
+    for (let i = 0; i < match.length && i < newWord.length; i++) {
+      result +=
+        match[i] === match[i].toUpperCase()
+          ? newWord[i].toUpperCase()
+          : newWord[i].toLowerCase()
+    }
+    return result
+  })
 }
