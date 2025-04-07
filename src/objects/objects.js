a612455f3d449c2e1ec0640815318f19de771876
ketace06 <ismail.omaramin06@gmail.com>
2025-04-07 14:43:17 +0200

    feat:

--- src/objects/objects.js
+++ src/objects/objects.js
@@ -1,6 +1,6 @@
 /**
- * In JavaScript objects are really diffrent form Java or other object based languages.
- * The syntax to use it looks more like declaring structs, but the can also carry methods.
+ * In JavaScript objects are really different from Java or other object-based languages.
+ * The syntax to use it looks more like declaring structs, but they can also carry methods.
  * https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#objects
  */
@@ -10,7 +10,11 @@
  * The values of the properties should be "Toto", "Tutu"
  */
 export function crateUserObject() {
-  // Write your code here
+  const newObjectName = {
+    first_name: 'Toto',
+    last_name: 'Tutu',
+  }
+  return newObjectName
 }
 
 /**
@@ -19,7 +23,8 @@ export function crateUserObject() {
  * @return string a concatenation of the first and last name, separated with a space
  */
 export function accessPropertiesInObjects(object) {
-  // Write your code here
+  const fullName = `${object.first_name} ${object.last_name}`
+  return fullName
 }
 
 /**
@@ -30,7 +35,9 @@ export function accessPropertiesInObjects(object) {
  * and all original object values mapped to lower case
  */
 export function iteratesThroughObjectValuesAndProperties(object) {
-  // Write your code here
+  const keys = Object.keys(object).map((key) => key.toUpperCase())
+  const values = Object.values(object).map((value) => value.toLowerCase())
+  return { keys, values }
 }
 
 /**
@@ -39,23 +46,40 @@ export function iteratesThroughObjectValuesAndProperties(object) {
  * @return {{younger: string, older: string}}
  */
 export function retrieveMaximumMinimumUserAges(users) {
-  // Write your code here
+  if (users.length === 0) return { younger: '', older: '' }
+
+  let youngerUser = users[0]
+  let olderUser = users[0]
+
+  for (const user of users) {
+    if (user.age < youngerUser.age) {
+      youngerUser = user
+    }
+    if (user.age >= olderUser.age) {
+      olderUser = user
+    }
+  }
+
+  return {
+    younger: youngerUser.name,
+    older: olderUser.name,
+  }
 }
 
 /**
  * In javascript, objects can be represented as string, this is JSON
  * https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
  * @param {string} string
- * @return {Object} An object retried by parsing the string
+ * @return {Object} An object retrieved by parsing the string
  */
 export function parseJavaScriptObjectNotation(string) {
-  // Write your code here
+  return JSON.parse(string)
 }
 
 /**
  * @param {Object} object
- * @return {string} An string representing the given object
+ * @return {string} A string representing the given object
  */
 export function stringifyJavaScriptObjectNotation(object) {
-  // Write your code here
+  return JSON.stringify(object)
 }
