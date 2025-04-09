e8ff3e6b445e53a2f5d54ad7c5094ac95cbb3a6d
ketace06 <ismail.omaramin06@gmail.com>
2025-04-09 09:19:48 +0200

    feat: implement basic fetch from GitHub Octocat API

--- src/fetch/fetchData.js
+++ src/fetch/fetchData.js
@@ -3,8 +3,14 @@
  * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
  * In the next exercise, when the user clicks ont the button with id "click-to-fetch"
  * you have to call this url : https://api.github.com/octocat
- * Then, you have to display the content returned by the api on the "pre" tag with id "display-here"
+ * Then, you have to display the content returned by the api on the "pre" tag with id "display-here" // pre is a preformatted text element
  */
 export function fetchDataOnClick() {
-  // Write your code here
+  document.querySelector('#click-to-fetch').addEventListener('click', () => {
+    fetch('https://api.github.com/octocat')
+      .then((response) => response.text())
+      .then((data) => {
+        document.querySelector('#display-here').textContent = data
+      })
+  })
 }
